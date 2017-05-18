005fa4930299fdedeed9d6e0ec963021beb881df
whoareyou101 <jisu2490@gmail.com>
2017-05-18 10:30:21 +0900

    VersionCPT del

--- workspace/VersionCPT/src/view/FileAddPane.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import javax.swing.JPanel;
-
-public class FileAddPane extends JPanel{
-
-	
-	
-}
--- workspace/VersionCPT/src/view/VerAddPane.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-import javax.swing.JPanel;
-
-public class VerAddPane  extends JPanel{
-
-}
